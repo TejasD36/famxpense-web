47783d5f21b3cf618fa61dc2fc2ec2885dca7fa6
TejasD36 <tejas_dudhal@bpointer.in>
2026-07-01 13:16:30 +0530

    test 4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "65350923" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1512337693" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
