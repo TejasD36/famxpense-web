a350f3a88b2ee106501d11f539189cd2e16fe44a
TejasD36 <tejas_dudhal@bpointer.in>
2026-09-23 15:31:18 +0530

    feat: lottie added and existing flow refined

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,8 +35,22 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27da0","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
+
+const removeAppLoader = () => {
+  const loader = document.getElementById('app-loader');
+  if (!loader) return;
+  loader.classList.add('is-hidden');
+  window.setTimeout(() => loader.remove(), 260);
+};
+
+window.addEventListener('flutter-first-frame', removeAppLoader, {
+  once: true,
+});
+
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "1762737475" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
-  }
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+    window.setTimeout(removeAppLoader, 2500);
+  },
 });
